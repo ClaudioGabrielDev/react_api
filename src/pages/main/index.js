cf40845ac3c9076d65cca0a1595cbb91a62ced54
Claudio Gabriel <designgabriel15@gmail.com>
2020-08-24 10:33:26 -0300

    Terceiro Commit
    Criando Paginação virtual em LocalHost

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,8 @@ export default class Main extends Component {
 
     state = {
         heros: [],
+        heroinfo: [],
+        offset: 0,
 
     };
 
@@ -14,18 +16,50 @@ export default class Main extends Component {
 
     }
 
-    loadHeros = async () => {
-        const response = await api.get('');
+    loadHeros = async (offset = 0) => {
+        const response = await api.get(`https://gateway.marvel.com:443/v1/public/characters?limit=10&ts=1597841898&apikey=3dd643db72bfef60e2ba5654f10c3070&hash=ada0c5002b1f3db5cb18a7eaa9ee42d4&offset=${offset}`);
 
-        this.setState({ heros: response.data.data.results })
+        const { results, ...heroinfo } = response.data.data;
+
+        this.setState({ heros: results, heroinfo, offset });
+    };
+
+    
+
+    prevPage = () => {
+    
+        const { offset, heroinfo} = this.state;
+
+        if (offset === 0) return;
+
+        const offNumber = offset - 10;
+
+        this.loadHeros(offNumber);
+        
+    }
+    nextPage = () => {
+        const { offset, heroinfo} = this.state;
+
+        if (offset === heroinfo.total) return;
+
+        const offNumber = offset + 10;
+
+        this.loadHeros(offNumber);
     };
 
     render(){
 
-        const { heros } = this.state;
+        const { heros, offset, heroinfo } = this.state;
 
         return (
-            <div className="list-heros">{heros.map(hero => (
+            
+            <div className="list-heros">
+            <div className="actions">
+                <button disabled= {offset === 0 } onClick = {this.prevPage}>Anterior</button>
+                <button disabled= {offset === heroinfo.total } onClick = {this.nextPage}>Proximo</button>
+            </div>
+                
+                {heros.map(hero => (
                 <article key={hero.id}>
                     <div className="img-hero"><img src={hero.thumbnail.path + '.' + hero.thumbnail.extension} alt="img do heroi"></img></div>
                     <div className="info-heros">
@@ -33,7 +67,13 @@ export default class Main extends Component {
                     <a href="#">Descrição</a>
                     </div>
                     </article>
-            ))}</div>
+            ))}
+                <div className="actions">
+                    <button disabled= {offset === 0 } onClick = {this.prevPage}>Anterior</button>
+                    <button disabled= {offset === heroinfo.total } onClick = {this.nextPage}>Proximo</button>
+                </div>
+            </div>
+
         );
     }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL:'https://gateway.marvel.com:443/v1/public/characters?limit=10&offset=0&ts=1597841898&apikey=3dd643db72bfef60e2ba5654f10c3070&hash=ada0c5002b1f3db5cb18a7eaa9ee42d4'});
+const api = axios.create({baseURL:'https://gateway.marvel.com:443/v1/public/characters?limit=10&ts=1597841898&apikey=3dd643db72bfef60e2ba5654f10c3070&hash=ada0c5002b1f3db5cb18a7eaa9ee42d4'});
 
 export default api;
