b9e780b67ea6821a16d56bdbb1070bb24c9e2184
Claudio Gabriel <designgabriel15@gmail.com>
2020-08-24 22:10:39 -0300

    Quarto Commit
    Acrescentando opção de mostrar as descrição do personagem e começando a implementar a função de buscar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './style.css'
 
 const Header = () => (
-    <header id="main-header">Marvel</header>
+    <header id="main-header">
+            Marvel
+        </header>
 );
     
 export default Header;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,6 +8,8 @@ export default class Main extends Component {
         heros: [],
         heroinfo: [],
         offset: 0,
+        ShowMe: false,
+        nameStartsWith: [],
 
     };
 
@@ -16,12 +18,12 @@ export default class Main extends Component {
 
     }
 
-    loadHeros = async (offset = 0) => {
+    loadHeros = async (offset = 0, nameStartsWith) => {
         const response = await api.get(`https://gateway.marvel.com:443/v1/public/characters?limit=10&ts=1597841898&apikey=3dd643db72bfef60e2ba5654f10c3070&hash=ada0c5002b1f3db5cb18a7eaa9ee42d4&offset=${offset}`);
 
         const { results, ...heroinfo } = response.data.data;
 
-        this.setState({ heros: results, heroinfo, offset });
+        this.setState({ heros: results, heroinfo, offset, nameStartsWith });
     };
 
     
@@ -36,7 +38,7 @@ export default class Main extends Component {
 
         this.loadHeros(offNumber);
         
-    }
+    };
     nextPage = () => {
         const { offset, heroinfo} = this.state;
 
@@ -46,6 +48,17 @@ export default class Main extends Component {
 
         this.loadHeros(offNumber);
     };
+    descrip(){
+        this.setState({
+            ShowMe:!this.state.ShowMe
+        })
+    }
+
+    
+
+
+    
+    
 
     render(){
 
@@ -53,19 +66,29 @@ export default class Main extends Component {
 
         return (
             
+            
+            
             <div className="list-heros">
-            <div className="actions">
-                <button disabled= {offset === 0 } onClick = {this.prevPage}>Anterior</button>
-                <button disabled= {offset === heroinfo.total } onClick = {this.nextPage}>Proximo</button>
-            </div>
+
+                <div className="input-heros">
+                    
+                    <input type="text" placeholder="Nome do herói" className="form-heros" id="name-hero"></input>
+                    <button className="bt-input" type="text" >Pesquisar</button>
+                </div>
+            
                 
                 {heros.map(hero => (
                 <article key={hero.id}>
                     <div className="img-hero"><img src={hero.thumbnail.path + '.' + hero.thumbnail.extension} alt="img do heroi"></img></div>
                     <div className="info-heros">
                     <strong>{hero.name}</strong>
-                    <a href="#">Descrição</a>
+                    <button className="bt-info" onClick = {() => this.descrip()}>Descrição</button>
                     </div>
+                    {this.state.ShowMe?
+                        <div className="description">{hero.description}</div>
+                    :null
+                    }
+                    
                     </article>
             ))}
                 <div className="actions">
